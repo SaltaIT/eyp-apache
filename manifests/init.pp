a90dd3cc8addbab130eda261e622a917140c56de
Jordi Prats <jprats@systemadmin.es>
2016-02-01 12:14:25 +0100

    tabs2spaces & puppet lint

--- manifests/init.pp
+++ manifests/init.pp
@@ -36,114 +36,114 @@
 # Copyright 2015 Your name here, unless otherwise noted.
 #
 class apache (
-		$mpm=$apache::params::mpm_default,
-		$servertokens=$apache::params::servertokens_default,
-		$timeout=$apache::params::timeout_default,
-		$keepalive=$apache::params::keepalive_default,
-		$keepalivetimeout=$apache::params::keepalivetimeout_default,
-		$maxkeepalivereq=$apache::params::maxkeepalivereq_default,
-		$extendedstatus=$apache::params::extendedstatus_default,
-		$serversignature=$apache::params::serversignature_default,
-		$listen=[ '80' ],
-		$namevirtualhosts=undef,
-		$ssl=false,
-		$sni=true,
-		$trace=false,
-	)inherits apache::params {
+    $mpm=$apache::params::mpm_default,
+    $servertokens=$apache::params::servertokens_default,
+    $timeout=$apache::params::timeout_default,
+    $keepalive=$apache::params::keepalive_default,
+    $keepalivetimeout=$apache::params::keepalivetimeout_default,
+    $maxkeepalivereq=$apache::params::maxkeepalivereq_default,
+    $extendedstatus=$apache::params::extendedstatus_default,
+    $serversignature=$apache::params::serversignature_default,
+    $listen=[ '80' ],
+    $namevirtualhosts=undef,
+    $ssl=false,
+    $sni=true,
+    $trace=false,
+  )inherits apache::params {
 
-	validate_array($listen)
+  validate_array($listen)
 
-	if($namevirtualhosts)
-	{
-		validate_array($namevirtualhosts)
-	}
+  if($namevirtualhosts)
+  {
+    validate_array($namevirtualhosts)
+  }
 
-	package { $apache::params::packagename:
-		ensure => "installed",
-		notify => Service[$apache::params::servicename],
-	}
+  package { $apache::params::packagename:
+    ensure => "installed",
+    notify => Service[$apache::params::servicename],
+  }
 
-	if($apache::params::packagenamedevel)
-	{
-		package { $apache::params::packagenamedevel:
-			ensure => "installed",
-		}
-	}
+  if($apache::params::packagenamedevel)
+  {
+    package { $apache::params::packagenamedevel:
+      ensure => "installed",
+    }
+  }
 
-	file { "${apache::params::baseconf}/conf.d":
-		ensure => 'directory',
-		owner => 'root',
-		group => 'root',
-		mode => '0755',
-		require => Package[$apache::params::packagename],
-	}
+  file { "${apache::params::baseconf}/conf.d":
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => Package[$apache::params::packagename],
+  }
 
-	file { "${apache::params::baseconf}/conf.d/sites":
-		ensure => 'directory',
-		owner => 'root',
-		group => 'root',
-		mode => '0755',
-		require => File["${apache::params::baseconf}/conf.d"],
-	}
+  file { "${apache::params::baseconf}/conf.d/sites":
+    ensure  => 'directory',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0755',
+    require => File["${apache::params::baseconf}/conf.d"],
+  }
 
-	file { "${apache::params::baseconf}/${apache::params::conffile}":
-		ensure => 'present',
-		owner => 'root',
-		group => 'root',
-		mode => '0644',
-		content => template("apache/${apache::params::conftemplate}"),
-		require => File["${apache::params::baseconf}/conf.d/sites"],
-		notify => Service[$apache::params::servicename],
-	}
+  file { "${apache::params::baseconf}/${apache::params::conffile}":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => template("apache/${apache::params::conftemplate}"),
+    require => File["${apache::params::baseconf}/conf.d/sites"],
+    notify  => Service[$apache::params::servicename],
+  }
 
-	concat { "${apache::params::baseconf}/conf.d/modules.conf":
-		ensure  => 'present',
-		owner   => 'root',
-		group   => 'root',
-		mode    => '0644',
-		require => Package[$apache::params::packagename],
-		notify  => Service[$apache::params::servicename],
-	}
+  concat { "${apache::params::baseconf}/conf.d/modules.conf":
+    ensure  => 'present',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    require => Package[$apache::params::packagename],
+    notify  => Service[$apache::params::servicename],
+  }
 
-	concat::fragment { "loadmodule header":
-		target  => "${apache::params::baseconf}/conf.d/modules.conf",
-		order   => '00', #answer to life the universe and everything
-		content => "#puppet managed file\n",
-	}
+  concat::fragment { "loadmodule header":
+    target  => "${apache::params::baseconf}/conf.d/modules.conf",
+    order   => '00', #answer to life the universe and everything
+    content => "#puppet managed file\n",
+  }
 
-	if($ssl)
-	{
-		package { $apache::params::modssl_package:
-			ensure => 'installed',
-		}
+  if($ssl)
+  {
+    package { $apache::params::modssl_package:
+      ensure => 'installed',
+    }
 
-		file { "${apache::params::baseconf}/ssl":
-			ensure => 'directory',
-			owner => 'root',
-			group => 'root',
-			mode => '0755',
-			require => Package[$apache::params::packagename],
-		}
+    file { "${apache::params::baseconf}/ssl":
+      ensure  => 'directory',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0755',
+      require => Package[$apache::params::packagename],
+    }
 
-		file { "${apache::params::baseconf}/conf.d/ssl.conf":
-			ensure  => 'present',
-			owner   => 'root',
-			group   => 'root',
-			mode    => '0644',
-			require =>  [
-										File["${apache::params::baseconf}/conf.d"],
-										Package[[$apache::params::packagename, $apache::params::modssl_package]]
-									],
-			notify  => Service[$apache::params::servicename],
-			content => template("${module_name}/ssl/ssl.erb"),
-		}
-	}
+    file { "${apache::params::baseconf}/conf.d/ssl.conf":
+      ensure  => 'present',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+      require =>  [
+                    File["${apache::params::baseconf}/conf.d"],
+                    Package[[$apache::params::packagename, $apache::params::modssl_package]]
+                  ],
+      notify  => Service[$apache::params::servicename],
+      content => template("${module_name}/ssl/ssl.erb"),
+    }
+  }
 
-	service { $apache::params::servicename:
-		name => $apache::params::servicename,
-		enable => true,
-		ensure => "running",
-		require => File["${baseconf}/${apache::params::conffile}"],
-	}
+  service { $apache::params::servicename:
+    name    => $apache::params::servicename,
+    enable  => true,
+    ensure  => "running",
+    require => File["${baseconf}/${apache::params::conffile}"],
+  }
 
 }
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,75 +1,75 @@
 class apache::params {
 
-	$servertokens_default="Prod"
-	$timeout_default=30
-	$keepalive_default=true
-	$keepalivetimeout_default=1
-	$maxkeepalivereq_default=1000
-	$extendedstatus_default=true
-	$mpm_default="prefork"
-	$serversignature_default=false
+  $servertokens_default="Prod"
+  $timeout_default=30
+  $keepalive_default=true
+  $keepalivetimeout_default=1
+  $maxkeepalivereq_default=1000
+  $extendedstatus_default=true
+  $mpm_default="prefork"
+  $serversignature_default=false
 
-	case $::osfamily
-	{
-		'redhat':
-    		{
-					$baseconf='/etc/httpd'
-					$modulesdir='modules'
-					$loadmodules_extra=true
-					$apache_username='apache'
-					$load_mpm_prefork=false
-					$apache24=false
-					$modssl_package= [ 'mod_ssl' ]
+  case $::osfamily
+  {
+    'redhat':
+        {
+          $baseconf='/etc/httpd'
+          $modulesdir='modules'
+          $loadmodules_extra=true
+          $apache_username='apache'
+          $load_mpm_prefork=false
+          $apache24=false
+          $modssl_package= [ 'mod_ssl' ]
 
-					$fastcgi_dependencies= [ 'make', 'gcc', 'gcc-c++' ]
+          $fastcgi_dependencies= [ 'make', 'gcc', 'gcc-c++' ]
 
 
-			case $::operatingsystemrelease
-			{
-				/^[67].*$/:
-				{
-					$packagename='httpd'
-					$packagenamedevel='httpd-devel'
-					$servicename='httpd'
-					$conftemplate='httpdconfcentos6.erb'
-					$conffile='conf/httpd.conf'
-				}
-				default: { fail("Unsupported RHEL/CentOS version!")  }
-			}
-		}
-		'Debian':
-		{
-			#
-			# QUICK & DIRTY
-			#
+      case $::operatingsystemrelease
+      {
+        /^[67].*$/:
+        {
+          $packagename='httpd'
+          $packagenamedevel='httpd-devel'
+          $servicename='httpd'
+          $conftemplate='httpdconfcentos6.erb'
+          $conffile='conf/httpd.conf'
+        }
+        default: { fail("Unsupported RHEL/CentOS version!")  }
+      }
+    }
+    'Debian':
+    {
+      #
+      # QUICK & DIRTY
+      #
 
-			$baseconf='/etc/apache2'
-			$modulesdir='/usr/lib/apache2/modules'
-			$loadmodules_extra=false
-			$apache_username='www-data'
-			$load_mpm_prefork=true
-			$apache24=true
+      $baseconf='/etc/apache2'
+      $modulesdir='/usr/lib/apache2/modules'
+      $loadmodules_extra=false
+      $apache_username='www-data'
+      $load_mpm_prefork=true
+      $apache24=true
 
-			case $::operatingsystem
-			{
-				'Ubuntu':
-				{
-					case $::operatingsystemrelease
-					{
-						/^14.*$/:
-						{
-							$packagename=[ 'apache2', 'apache2-mpm-prefork' ]
-							$servicename='apache2'
-							$conftemplate='httpdconfcentos6.erb'
-							$conffile='apache2.conf'
-						}
-						default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
-					}
-				}
-				'Debian': { fail("Unsupported")  }
-				default: { fail("Unsupported Debian flavour!")  }
-			}
-		}
-		default: { fail("Unsupported OS!")  }
-	}
+      case $::operatingsystem
+      {
+        'Ubuntu':
+        {
+          case $::operatingsystemrelease
+          {
+            /^14.*$/:
+            {
+              $packagename=[ 'apache2', 'apache2-mpm-prefork' ]
+              $servicename='apache2'
+              $conftemplate='httpdconfcentos6.erb'
+              $conffile='apache2.conf'
+            }
+            default: { fail("Unsupported Ubuntu version! - $::operatingsystemrelease")  }
+          }
+        }
+        'Debian': { fail('Unsupported')  }
+        default: { fail('Unsupported Debian flavour!')  }
+      }
+    }
+    default: { fail('Unsupported OS!')  }
+  }
 }
