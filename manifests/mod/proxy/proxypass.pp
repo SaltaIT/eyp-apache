e0139c6308e71fbb494a8774a139c5ef119827a6
Jordi Prats <jprats@systemadmin.es>
2016-04-25 14:30:58 +0200

    lint

--- manifests/mod/proxy/proxypass.pp
+++ manifests/mod/proxy/proxypass.pp
@@ -1,9 +1,9 @@
 define apache::mod::proxy::proxypass(
                                       $servername,
                                       $destination,
-                                      $vhost_order      = '00',
-                                      $port             = '80',
-                                      $url        = $name,
+                                      $vhost_order = '00',
+                                      $port        = '80',
+                                      $url         = $name,
                                     ) {
   #
   concat::fragment{ "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf proxypass ${url} ${destination}":
