dc652db59ca3cc8a141aaaf653bbed2251ba6769
Jordi Prats <jprats@systemadmin.es>
2016-05-12 17:56:45 +0200

    bugfix

--- manifests/nss/csr.pp
+++ manifests/nss/csr.pp
@@ -22,7 +22,7 @@ define apache::nss::csr (
   # C=RC
 
   # certutil -R -s "CN=www.hereyourname.com, O=Networking4all B.V., L=Amsterdam, ST=Noord-Holland, C=NL" -o mycert.req -a -g 2048 -d /here/your/path/
-  exec { "nss csr ${certdb} ${keysize} ${aliasname} ${name} ${country} ${state} ${location} ${organization} ${cn}":
+  exec { "nss csr ${certdb} ${keysize} ${aliasname} ${name} ${country} ${state} ${locality} ${organization} ${cn}":
     command => "strings /dev/urandom | certutil -R -s 'CN=${cn}, O=${organization}, OU=${organization_unit}, L=${locality}, ST=${state}, C=${country}' -o ${apache::params::baseconf}/ssl/${aliasname}.csr -a -g ${keysize} -d ${certdb} -f ${certdb}/pwdfile.txt",
     creates => "${apache::params::baseconf}/ssl/${aliasname}.csr",
     require => [
@@ -37,7 +37,7 @@ define apache::nss::csr (
     owner   => 'root',
     group   => 'root',
     mode    => '0644',
-    require => Exec["nss csr ${certdb} ${keysize} ${aliasname} ${name} ${country} ${state} ${location} ${organization} ${cn}"],
+    require => Exec["nss csr ${certdb} ${keysize} ${aliasname} ${name} ${country} ${state} ${locality} ${organization} ${cn}"],
   }
 
 
