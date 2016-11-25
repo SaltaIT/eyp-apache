23925d30c013b428b68059a0c6b55529efc54b3c
Jordi Prats <jprats@systemadmin.es>
2016-11-25 17:15:54 +0100

    detalls

--- manifests/mod/authz/svn.pp
+++ manifests/mod/authz/svn.pp
@@ -8,6 +8,7 @@ class apache::mod::authz::svn(
     apache::module { 'authz_svn_module':
       sofile  => "${apache::params::modulesdir}/mod_authz_svn.so",
       require => Class['::apache::mod::davsvn'],
+      order   => '99',
     }
   }
 }
