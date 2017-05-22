95fd9090f5dffdca560019d5a4df4eee349a10be
Jordi Prats <jprats@systemadmin.es>
2017-05-22 14:38:14 +0200

    canvi iordre browsermatch

--- manifests/browsermatch.pp
+++ manifests/browsermatch.pp
@@ -11,6 +11,6 @@ define apache::browsermatch (
   concat::fragment{ "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf.run browsermatch ${regex}":
     target  => "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf.run",
     content => template("${module_name}/browsermatch/browsermatch.erb"),
-    order   => '19',
+    order   => '31',
   }
 }
