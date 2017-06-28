3df3c58dbed8f989ba65057801bbce7eb28f047b
Jordi Prats <jprats@systemadmin.es>
2017-06-28 20:30:21 +0200

    typo

--- manifests/alias.pp
+++ manifests/alias.pp
@@ -6,7 +6,7 @@ define apache::alias(
                       $port        = '80',
                     ) {
 
-  concat::fragment{ "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf.run alias ${alias} ${dir}":
+  concat::fragment{ "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf.run alias ${alias_from} ${alias_dir}":
     target  => "${apache::params::baseconf}/conf.d/sites/${vhost_order}-${servername}-${port}.conf.run",
     content => template("${module_name}/aliases/alias.erb"),
     order   => '13',
