8aa0cd5d6dd1b26aab396677d6918a8b9e7b5a18
Jordi Prats <jordi.prats@gmail.com>
2018-02-22 16:19:40 +0100

    lint example

--- examples/adauth.pp
+++ examples/adauth.pp
@@ -28,28 +28,27 @@ apache::vhost {'et2blog':
 # AuthUserFile /dev/null
 # Require valid-user
 apache::vhost::adauth { 'et2blog':
-  url => '/',
-  auth_ldap_url => 'ldaps://srv-ad02.nttcom.ms.local/OU=NTTCMS,DC=nttcom,DC=ms,DC=local?sAMAccountName?sub?(objectClass=user)',
-  auth_ldap_bind_dn => 'cn=auth GBM,OU=Service Account,OU=NTTCMS,DC=nttcom,DC=ms,DC=local',
+  url                     => '/',
+  auth_ldap_url           => 'ldaps://srv-ad02.nttcom.ms.local/OU=NTTCMS,DC=nttcom,DC=ms,DC=local?sAMAccountName?sub?(objectClass=user)',
+  auth_ldap_bind_dn       => 'cn=auth GBM,OU=Service Account,OU=NTTCMS,DC=nttcom,DC=ms,DC=local',
   auth_ldap_bind_password => 'XXXXXXXX',
 }
 
 apache::vhost {'testing.lol':
-        order                  => '77',
-        serveradmin            => 'root@lolcathost.lol',
-        serveralias            => [ '1.testing.lol', '2.testing.lol' ],
-        documentroot           => '/var/www/testing/',
-        options                => [ 'Indexes', 'FollowSymLinks', 'MultiViews' ],
-        rewrites               => [ 'RewriteCond %{HTTP_HOST} !^testing\.lol', 'RewriteRule ^/(.*)$ http://www\.testing\.lol/$1 [R=301,L]' ],
-        aliasmatch             => { 'RUC/lol' => '/var/www/testing/hc.php',
-                                  '(.*)' => '/var/www/testing/cc.php'},
-        scriptalias            => { '/cgi-bin/' => '"/var/www/testing/cgi-bin/"' },
-        directoryindex         => [ 'index.php', 'lolindex.php', 'lol.html' ],
+  order          => '77',
+  serveradmin    => 'root@lolcathost.lol',
+  serveralias    => [ '1.testing.lol', '2.testing.lol' ],
+  documentroot   => '/var/www/testing/',
+  options        => [ 'Indexes', 'FollowSymLinks', 'MultiViews' ],
+  rewrites       => [ 'RewriteCond %{HTTP_HOST} !^testing\.lol', 'RewriteRule ^/(.*)$ http://www\.testing\.lol/$1 [R=301,L]' ],
+  aliasmatch     => { 'RUC/lol' => '/var/www/testing/hc.php','(.*)' => '/var/www/testing/cc.php'},
+  scriptalias    => { '/cgi-bin/' => '"/var/www/testing/cgi-bin/"' },
+  directoryindex => [ 'index.php', 'lolindex.php', 'lol.html' ],
 }
 
 apache::directory { 'testing.lol':
-                      directory     => '/var/www/testing/cgi-bin/',
-                      vhost_order   => '77',
-                      options       => [ '+ExecCGI', '-Includes' ],
-                      allowoverride => 'None',
+  directory     => '/var/www/testing/cgi-bin/',
+  vhost_order   => '77',
+  options       => [ '+ExecCGI', '-Includes' ],
+  allowoverride => 'None',
 }
